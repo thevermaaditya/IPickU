cf715c3aa4dcfb7c3690d030fbe6f41666d1cd11
thevermaaditya <as75189@gmail.com>
2024-12-31 17:07:52 +0530

    Done with time pass

--- index.js
+++ index.js
@@ -1,6 +1,31 @@
 const fan = document.querySelector(".fan");
 const text = document.querySelector(".text p");
 
+
+//--------------------------------------------
+// function isOverlapping(element1, element2) {
+//     const rect1 = element1.getBoundingClientRect();
+//     const rect2 = element2.getBoundingClientRect();
+
+//     return !(
+//         rect1.top > rect2.bottom ||
+//         rect1.bottom < rect2.top ||
+//         rect1.left > rect2.right ||
+//         rect1.right < rect2.left
+//     );
+// }
+
+// function updateTextColor() {
+//     const words = text.innerText.split(' ');
+//     text.innerHTML = words.map(word => {
+//         const span = document.createElement('span');
+//         span.textContent = word + ' ';
+//         span.style.color = isOverlapping(fan, span) ? 'white' : '#c2a789';
+//         return span.outerHTML;
+//     }).join('');
+// }
+//--------------------------------------------
+
 if (!fan || !text) {
     console.error("Required elements not found in the DOM.");
 } else {
@@ -24,12 +49,17 @@ if (!fan || !text) {
         fan.style.left = e.pageX + "px";
         fan.style.top = e.pageY + "px";
         fan.style.transitionDuration = "0.1s";
+        // updateTextColor();
     });
     document.addEventListener('touchmove', (e) => {
-        fan.style.left = e.pageX + "px";
-        fan.style.top = e.pageY + "px";
+        // e.preventDefault();
+        console.log(e.touches);
+        fan.style.left = e.touches[0].clientX + "px";
+        fan.style.top = e.touches[0].clientY + "px";
         fan.style.transitionDuration = "0.1s";
+        // updateTextColor();
     });
-
+    
     changeText();
 }
+
